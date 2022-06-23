1d719b275d62b4fea865326a866e4d38202ff47c
Vera DOS SANTOS <vera.dossantos@newbound.eu>
2022-06-23 10:47:57 +0200

    Création des models booking et client

--- src/models/bookingModel.ts
+++ src/models/bookingModel.ts
@@ -1,2 +1,49 @@
 import dbConnection from "../config/dbConfig";
 import Sequelize from "sequelize";
+
+const bookingModel = dbConnection.define("booking", {
+    idUbooking: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    creationDate: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    appointementDate: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    nbHours: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+    },
+    description: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    accepted: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+    },
+    totalPrice: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    },
+    cancelDate: {
+        type: Sequelize.DATE,
+        allowNull: true
+    },
+    isCancelled: {
+        type: Sequelize.BOOLEAN,
+        allowNull: true
+    },
+    serviceDone: {
+        type: Sequelize.BOOLEAN,
+        allowNull: true
+    }
+});
+
+export default bookingModel;
