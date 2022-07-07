137cda128fe7fd3fd993f80dc7d2301a5a16cb8e
Vera DOS SANTOS <vera.dossantos@newbound.eu>
2022-07-07 12:08:03 +0200

    Fin de la création de la fonction pour récuperé les bookings passé à venir et present, accepté ou non par l'id du client

--- src/controllers/clientController.ts
+++ src/controllers/clientController.ts
@@ -2,12 +2,9 @@ import clientModel from "../models/clientModel";
 import userModel from "../models/userModel";
 import Client from "../types/clientType";
 import Express from "express";
-
 import bookingModel from "../models/bookingModel";
-
 import dbConnection from "../config/dbConfig";
 import { Transaction } from "sequelize/types";
-
 import moment from "moment";
 import { Op } from "sequelize";
 
@@ -28,9 +25,8 @@ const getClients = (req: Express.Request, res: Express.Response) => {
     });
 };
 
-
 //fonction permettant de modifier un client par le commercial et le client lui-même
-const editClient = async (req: Express.Request, res: Express.Response) => {
+const editClient = async (req: Express.Request | any, res: Express.Response) => {
   const { firstName, lastName, email, phone, address, postalCode, city } = req.body;
   //on initie la transaction
   const transaction: Transaction = await dbConnection.transaction();
@@ -42,7 +38,7 @@ const editClient = async (req: Express.Request, res: Express.Response) => {
       email: email
     }, {
       where: {
-        id: req.params.id
+        id: req.userId
       }, individualHooks: true
     }, { transaction: transaction });
 
@@ -54,7 +50,7 @@ const editClient = async (req: Express.Request, res: Express.Response) => {
       city: city
     }, {
       where: {
-        idUser: req.params.id
+        idUser: req.userId
       }, individualHooks: true
     }, { transaction: transaction });
 
@@ -68,15 +64,14 @@ const editClient = async (req: Express.Request, res: Express.Response) => {
   }
 }
 
-
-  //Récupérer le client par l'id
+//Récupérer le client par l'id
 const getClientById = (req: Express.Request | any, res: Express.Response) => {
   userModel.findByPk(req.userId, {
     include: [
       {
         model: clientModel,
         where: {
-          idUser:  req.userId
+          idUser: req.userId
         }
       }
     ]
@@ -89,65 +84,51 @@ const getClientById = (req: Express.Request | any, res: Express.Response) => {
     });
 };
 
-
-//Récupérer les booking du client
-const getBookingByIdClient = (req: Express.Request, res: Express.Response) => {
-  clientModel.findByPK(
-    req.params.id,
-    {
-      include: [{
-        model: bookingModel,
-        where: {
-          idClient: req.params.id
-        }
-      }]
-    }
-  )
-    .then((client: Client) => {
-      res.status(201).json(client);
-    })
-    .catch((err: Error) => {
-      res.status(409).send(err);
-    });
-};
-
-
-const dateBooking = (req: Express.Request, res: Express.Response) => {
+//Fonction qui permet de récuperé les bookings du client par (future, present, passé) et (accepté ou pas)
+const getBookingById = (req: Express.Request | any, res: Express.Response) => {
   //permet de récuperé l'argument dans l'url
   const dateType = req.params.dateType;
   const accepted = req.params.accepted;
+  //On instancie les variable à null
   let whereClause = null;
+  let acceptedClause = null;
+  //Pour les dates on verifie si c'est un date supperieur à aujourd'hui, 
   if (dateType === "future") {
     whereClause = { [Op.gt]: moment().add(1, "d").format("YYYY-MM-DD") };
   }
-  //Sinon dans l'url === passed alors prend les Bookings passé
+  //Si c'est une date inférieur à aujourd'hui
   else if (dateType === "past") {
     whereClause = { [Op.lt]: moment().subtract(1, "d").format("YYYY-MM-DD") };
   }
-  //Sinon prend les bookings du jour
+  //Sinon si c'est une date égale à aujourd'hui
   else {
     whereClause = { [Op.between]: [moment().format("YYYY-MM-DD"), moment().add(1, "d").format("YYYY-MM-DD")] };
   }
-  let acceptedClause = null;
+  //Pour l'acceptation de la prestation :
   if (accepted === "true") {
     acceptedClause = true
   } else {
     acceptedClause = false
   }
-  clientModel.findByPK(
-    req.params.id,
-    {
-      include: [{
-        model: bookingModel,
-        attributes: ["appointementDate", "nbHours", "description", "totalPrice", "accepted"],
+  //On fait deux jointure dans la même requette
+  userModel.findByPk(req.userId, {
+    include: [
+      {
+        model: clientModel,
         where: {
-          idClient: req.params.id,
-          appointementDate: whereClause,
-          accepted: acceptedClause,
+          idUser: req.userId
+        },
+        include: {
+          model: bookingModel,
+          //attributes: ["appointementDate", "nbHours", "description", "totalPrice", "accepted"],
+          where: {
+            appointementDate: whereClause,
+            accepted: acceptedClause,
+          }
         }
-      }]
-    }
-  )
+      }
+    ]
+  })
     .then((client: Client) => {
       res.status(201).json(client);
     })
@@ -156,10 +137,6 @@ const dateBooking = (req: Express.Request, res: Express.Response) => {
     });
 };
 
-
-
-
-
-export { getClients, editClient, getBookingByIdClient, getClientById, dateBooking };
+export { getClients, editClient, getBookingById, getClientById };
 
 
--- src/routes/clientRoute.ts
+++ src/routes/clientRoute.ts
@@ -1,20 +1,18 @@
 import express from "express";
 const router = express.Router();
 import checkUser from "../middleware/authMiddleware";
-
-
-import { getClients, editClient, getBookingByIdClient, getClientById, dateBooking } from "../controllers/clientController";
+import { getClients, editClient, getBookingById, getClientById } from "../controllers/clientController";
 import { salesAddClient } from "../controllers/authController";
 
-
+//les get:
+router.get("/booking/:dateType(future|present|past)/:accepted(true|false)/", checkUser, getBookingById);
+router.get("/profile/", checkUser, getClientById);
 router.get("/", getClients);
-router.get("/:id", checkUser, getClientById);
 
-router.put("/:id", editClient);
+//les put:
+router.put("/", checkUser, editClient);
 
+//Les post:
 router.post("/sales/", salesAddClient);
-
-router.get("/booking/:id", getBookingByIdClient);
-router.get("/booking/:id/:dateType(future|present|past)/:accepted(true|false)", dateBooking)
 
 export default router;
