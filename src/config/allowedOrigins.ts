15370a90bb81532315a6df7351d23104ea58e189
niklas.edelstam <n.edelstam-ext@catalyse-it.com>
2025-02-09 18:42:23 +0100

    change originnis and allowPublicKeyRetrieval

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,12 +1,12 @@
 const allowedOrigins = [
-    "http://127.0.0.1:5500/",
-    "http://localhost:3500/",
-    "http://localhost:3000",
-    "http://localhost:3001/",
-    "http://marya.app",
-    "https://marya.app",
-    "http://marya.app/",
-    "https://marya.app/"
+    "http://172.17.0.1:8081",
+    "http://172.17.0.1:8081/",
+    "http://localhost:8081",
+    "http://localhost:8081/",
+    "http://84.234.16.7:8081",
+    "http://84.234.16.7:8081/",
+    "https://marya.niklasedelstam.dev",
+    "https://marya.niklasedelstam.dev/"
 ];
 
 export default allowedOrigins;
--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -5,6 +5,9 @@ const {DB_HOST, DB_DATABASE,DB_PASSWORD,DB_PORT,DB_USER} = process.env;
 //permet de se connecter à la base de donnée
 const dbConnection = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
     dialect: "mariadb",
+    dialectOptions: {
+      allowPublicKeyRetrieval: true,
+  },
     host: DB_HOST,
     port: DB_PORT != undefined ? parseInt(DB_PORT): 3306,
     connectionLimit: 5
